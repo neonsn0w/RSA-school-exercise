f06d930ecd2b096d17733c1ee9ef4c915dad9a6e
neonsn0w <pietro@eprase.com>
2024-11-16 13:04:33 +0100

    User input

--- src/main/java/com/neonsn0w/Main.java
+++ src/main/java/com/neonsn0w/Main.java
@@ -1,16 +1,26 @@
 package com.neonsn0w;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.config.Configurator;
 
 public class Main {
-    public static void main(String[] args) {
-        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.INFO);
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.WARN);
+
+        System.out.print("Enter the message to encrypt: ");
+        String s = sc.nextLine();
         RSA rsa = new RSA();
-        String s = rsa.encrypt("The quick brown fox jumps over the lazy dog");
+        System.out.println("BEGINNING OF ENCRYPTED MESSSAGE");
+        s = rsa.encrypt(s);
         System.out.println(s);
-        System.out.println(rsa.decrypt(s));
+        System.out.println("END OF ENCRYPTED MESSSAGE");
+        System.out.println("Decrypted message: " + rsa.decrypt(s));
+        System.out.println("Public key: (" + rsa.getN() + ", " + rsa.getPublicExponentE() + ")");
+        System.out.println("Private key: (" + rsa.getN() + ", " + rsa.getPrivateExponentD() + ")");
     }
 }
